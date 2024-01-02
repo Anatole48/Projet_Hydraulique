e2255fe7f5b362035f784bd7ce9dc88aa57f843a
Anatole Briand <anatole.briand14@gmail.com>
2024-01-02 10:10:06 +0100

    Changement de CL pour une CL antisymetrique - Correction : Calcul du Flux puis mise a jour de la solution en 2 temps contre 1 avant - Creation d'une fonction condition_cfl pour la mise a jour du pas de temps - Ajout d'un fichier Conservation pour verfier la conservation de volume et d'energie du systeme.

--- Saint_Venant_B_Constante.py
+++ Saint_Venant_B_Constante.py
@@ -20,12 +20,14 @@ indice_choc=int((x_choc-x_L)/dx)
 #Les cellules 0 et Nx+1 sont des cellules fantomes qui servent de CL.
 W=np.zeros((2,Nx+2))
 X=np.zeros(Nx)
+F_vect=np.zeros((2,Nx+1))
 
-tmax=50
+tmax=5
 C=0.9
 
-duration = tmax
-dt_video=tmax/100
+#Données pour l'enregistrement de la video
+duration = tmax*10
+dt_video=0.01
 fps=tmax/(duration*dt_video)
 fig, ax = plt.subplots()
 
@@ -52,7 +54,6 @@ def F(W,i):
 def F_etoile(W,i):
 	F_etoile=np.zeros(2)
 	F_etoile=(Lambda_R(W,i+1)*F(W,i)-Lambda_L(W,i)*F(W,i+1)+Lambda_L(W,i)*Lambda_R(W,i+1)*(W[:,i+1]-W[:,i]))/(Lambda_R(W,i+1)-Lambda_L(W,i))
-	#print('Lambda : ',(Lambda_R(W,i+1)-Lambda_L(W,i)))
 	return F_etoile
 
 def Plot_water_high(X,W,temps):
@@ -62,11 +63,31 @@ def Plot_water_high(X,W,temps):
 	plt.xlim(-1.1,1.1)
 	plt.ylim(-0.1,2.1)
 	plt.title("Hauteur d'eau h en fonction de la position x")
-	plt.text(0.5,1.8,"Temps : "+str(round(temps,1)))
+	plt.text(0.5,1.8,"Temps : "+str(round(temps,2)))
 	plt.plot(X,W[0,1:Nx+1])
-	path_fig=image_folder+'/Saint_Venant_t_'+str(round(temps,1))+'.png'
+	path_fig=image_folder+'/Saint_Venant_t_'+str(round(temps,2))+'.png'
 	plt.savefig(path_fig)
 	image_path_list.append(path_fig)
+	plt.pause(0.001) #Pour afficher la hauteur d'eau durant le calcul
+
+def condition_cfl(W):
+	cfl=C*dx/(2*max(abs(Lambda_R(W,1)),abs(Lambda_L(W,0))))
+	for i in range(0,Nx+1):
+		cfl=min(cfl,C*dx/(2*max(abs(Lambda_R(W,i+1)),abs(Lambda_L(W,i)))))
+	return(cfl)
+
+def conservation(temps,W): #Ecrit le volume d'eau, l'energie potentiel, cinétique et mécanique dans un fichier pour vérifier leurs bonne conservation
+	volume_eau=0
+	energie_potentiel=0
+	enegie_cinetique=0
+	energie_mecanique=0
+	for i in range(1,Nx+1):
+		volume_eau=volume_eau+W[0,i]*dx
+		energie_potentiel=energie_potentiel+g*W[0,i]*dx
+		enegie_cinetique=0.5*((W[1,i]/W[0,i])**2)*dx
+	energie_mecanique=energie_mecanique+energie_potentiel+enegie_cinetique
+	conservation_file.write(str(temps)+" "+str(volume_eau)+" "+str(energie_potentiel)+" "+str(enegie_cinetique)+" "+str(energie_mecanique)+"\n")
+
 
 
 #CONDITION INITAL
@@ -96,48 +117,47 @@ image_path_list=[]
 # os.mkdir(image_folder)
 
 Plot_water_high(X,W,temps)
-plt.pause(0.1)
 
-dt=C*dx/(2*max(abs(Lambda_R(W,1)),abs(Lambda_L(W,0))))
-for i in range(0,Nx+1):
-	dt=min(dt,C*dx/(2*max(abs(Lambda_R(W,i+1)),abs(Lambda_L(W,i)))))
-temps=dt
+conservation_file=open("Conservation.txt", "w")
+conservation(temps,W)
 
+
+dt=condition_cfl(W)
+temps=temps+dt
 
 while temps<tmax:
-	W_inter=W
 
-	dt=C*dx/(2*max(abs(Lambda_R(W,1)),abs(Lambda_L(W,0))))
+
 	for i in range(0,Nx+1):
-		W_inter[:,i]=W[:,i]-dt*F_etoile(W,i)
-		W_inter[:,i+1]=W[:,i+1]+dt*F_etoile(W,i)
-		dt=min(dt,C*dx/(2*max(abs(Lambda_R(W,i+1)),abs(Lambda_L(W,i)))))
+		F_vect[:,i]=F_etoile(W,i)
+
+	for i in range(0,Nx+1):
+		W[:,i]=W[:,i]-dt*F_vect[:,i]/dx
+		W[:,i+1]=W[:,i+1]+dt*F_vect[:,i]/dx
 
 
 	#CONDITION AUX LIMITES
-	W_inter[0,0]=W_inter[0,1]
-	W_inter[0,Nx+1]=W_inter[0,Nx]
+	W[0,0]=W[0,1]
+	W[0,Nx+1]=W[0,Nx]
 
-	W_inter[1,0]=W_inter[1,1]
-	W_inter[1,Nx+1]=W_inter[1,Nx]
-
-
-	W=W_inter
+	W[1,0]=-W[1,1]
+	W[1,Nx+1]=-W[1,Nx]
 
 
 	if temps>seuil_temps_video:
 		seuil_temps_video=seuil_temps_video+dt_video
-		#make_frame(temps)
 		Plot_water_high(X,W,temps)
-		print('Je plot !')
-		plt.pause(0.001)
 		print(temps)
+		conservation(temps,W)
 
+	dt=condition_cfl(W)
 	temps=temps+dt
 
+
 Plot_water_high(X,W,temps)
-plt.pause(0.001)
 
 
 clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_path_list, fps=fps)
-clip.write_videofile(r"Saint_Venant_Animation.mp4",fps=30, codec="libx264")
+clip.write_videofile(r"Saint_Venant_Animation_CL3.mp4", codec="libx264")
+
+conservation_file.close()
