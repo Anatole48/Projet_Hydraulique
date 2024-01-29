8da61fd22d6555e71ab63500698d07dbda37815a
Anatole Briand <anatole.briand14@gmail.com>
2024-01-29 12:48:43 +0100

    Ajout de commentaires au modèle multicouches, mise en commentaire de la sauvegarde des plots et création d'une fonction plot.

--- Saint_Venant_B_Constant_n_couches.py
+++ Saint_Venant_B_Constant_n_couches.py
@@ -36,7 +36,19 @@ def condition_cfl(W):
 		cfl=min(cfl,C*dx/(2*max(abs(Lambda_R(W,i+1)),abs(Lambda_L(W,i)))))
 	return(cfl)
 
-def Plot_water_high(X,W,temps,k):
+def Plot_Water_High(X,W):
+	plt.clf()
+	plt.xlabel('Position x')
+	plt.ylabel(("Hauteur d'eau h"))
+	plt.xlim(-1.1,1.1)
+	plt.ylim(-0.1,3.1)
+	plt.title("Hauteur d'eau h en fonction de la position x")
+	plt.text(0,0.5,"Temps : "+str(round(temps,6)))
+	plt.text(0,0.2,"dt : "+str(round(dt,16)))
+	plt.plot(X,W[0,1:Nx+1])
+	plt.pause(0.001)
+
+def Save_Water_High_Plot(X,W,temps,k):
 	plt.clf()
 	plt.xlabel('Position x')
 	plt.ylabel(("Hauteur d'eau h"))
@@ -66,7 +78,7 @@ H_R=1
 dx=(x_R-x_L)/Nx
 x_choc=0
 
-Ny=5
+Ny=10
 
 
 
@@ -107,10 +119,10 @@ for i in range(0,Nx+2):
 fig, ax = plt.subplots()
 image_path_list=[]
 
-image_folder="/home/anatole/Images/Saint_Venant_Animation_n_couches" #Attention ce dossier est créé puis détruit à chaque execution
-if os.path.exists(image_folder):
-	shutil.rmtree(image_folder)
-os.mkdir(image_folder)
+# image_folder="/home/anatole/Images/Saint_Venant_Animation_n_couches" #Attention ce dossier est créé puis détruit à chaque execution
+# if os.path.exists(image_folder):
+# 	shutil.rmtree(image_folder)
+# os.mkdir(image_folder)
 
 
 
@@ -122,6 +134,7 @@ while temps<tmax and dt>pow(10,-15) and k<500:
 	W_inter[:,:,:]=0
 	dt=1000
 
+	#CREATION DE W_inter (regroupement des Ny Saint Venant Classiques), CALCULS DES FLUX ET DU PAS DE TEMPS
 	for j in range(0,Ny):
 		W_inter[0,:,j]=L[j]*W[0,:]
 		W_inter[1,:,j]=L[j]*W[j+1,:]
@@ -133,15 +146,20 @@ while temps<tmax and dt>pow(10,-15) and k<500:
 			dt=dt_inter
 	print(temps,' / ',dt)
 
+
+	#MISE A JOUR DES Ny Saint Venant Classique dans W_inter
 	for j in range(0,Ny):
 		for i in range(0,Nx+1):
 			W_inter[:,i,j]=W_inter[:,i,j]-dt*F_vect[:,i,j]/dx
 			W_inter[:,i+1,j]=W_inter[:,i+1,j]+dt*F_vect[:,i,j]/dx
 
+	#MISE A JOUR DE h^n+1 DANS W
 	W[:,:]=0
 	for j in range(0,Ny):
 		W[0,:]=W[0,:]+W_inter[0,:,j]
-	
+
+
+	#MISE A JOUR de h^n+1*u_j DANS W
 	G[0,:]=L[0]*(W[0,:]-W_inter[0,:,0])/dt
 	W[1,:]=W_inter[1,:,0]+dt*(W_inter[1,:,1]*np.maximum(zeros,G[0,:])+W_inter[1,:,0]*np.minimum(zeros,G[0,:]))/L[0]
 	for j in range(1,Ny-1):
@@ -149,7 +167,7 @@ while temps<tmax and dt>pow(10,-15) and k<500:
 		W[j+1,:]=W_inter[1,:,j]+dt*(W_inter[1,:,j+1]*np.maximum(zeros,G[j,:])+W_inter[1,:,j]*np.minimum(zeros,G[j,:])-W_inter[1,:,j]*np.maximum(zeros,G[j-1,:])-W_inter[1,:,j-1]*np.minimum(zeros,G[j-1,:]))/L[j]
 
 
-	#CONDITION AUX LIMITES
+	#CONDITION AUX LIMITES ANTISYMETRIQUES
 	W[0,0]=W[0,1]
 	W[0,Nx+1]=W[0,Nx]
 
@@ -159,15 +177,19 @@ while temps<tmax and dt>pow(10,-15) and k<500:
 
 
 	
-	#Plot water level
-	Plot_water_high(X,W,temps,k)
+	#SAUVEGARDE DU PLOT DANS LE DOSSIER "image folder"
+	#Plot_water_high(X,W,temps,k)
 	k=k+1
+
+	#JUSTE PLOT
+	Plot_Water_High(X,W)
 
 
 	temps=temps+dt
 
 
-duration = 10
-fps=len(image_path_list)/duration
-clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_path_list, fps=fps)
-clip.write_videofile(r"Saint_Venant_Animation_"+str(Ny)+"_couches.mp4", codec="libx264")
+#CREATION DE LA VIDEO
+# duration = 10
+# fps=len(image_path_list)/duration
+# clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_path_list, fps=fps)
+# clip.write_videofile(r"Saint_Venant_Animation_"+str(Ny)+"_couches.mp4", codec="libx264")
